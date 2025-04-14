71129c6c3aae26e2e2aa869e8b424703fc9a1069
la030902 <lteoh@wisc.edu>
2025-04-14 02:24:15 -0500

    added let video

---
+++ let video;.java
@@ -0,0 +1,43 @@
+let video;
+let classifier;
+let label = 'waiting...';
+
+// STEP 1: Load the model!
+let modelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+function setup() {
+  createCanvas(640, 520);
+  video = createCapture(VIDEO);
+  video.size(640, 520);
+  video.hide();
+
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height - 16);
+}
+
+// STEP 3: Get classification result
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  label = results[0].label;
+  classifyVideo();
+}
