c5eb294250a8ddabda3f8b3242eb0785e4699291
la030902 <lteoh@wisc.edu>
2025-04-14 02:17:51 -0500

    added sketch

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let video;
+let classifier;
+let label = 'waiting...';
+let stopImg, rainbowImg, ukuleleImg;
+
+
+function setup() {
+  let canvas = createCanvas(640, 520);
+  canvas.parent("canvas-container");
+
+  video = createCapture(VIDEO);
+  video.size(640, 520);
+  video.hide();
+
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0);
+
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label, width / 2, height - 16);
+
+  function preload() {
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bXy2kDNi/model.json');
+    stopImg = loadImage("images/stop.webp");
+    yieldImg = loadImage("images/yield.jpg");
+    streetImg = loadImage("images/street.jpg");
+  };
+  
+  if (label === "Stop") {
+    image(stopImg, 100, 100, 100, 100);
+  } else if (label === "Yield") {
+    image(yieldImg, 100, 100, 100, 100);
+  } else if (label === "Street") {
+    image(streetImg, 100, 100, 100, 100);
+  }
+}
+
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  label = results[0].label;
+  classifyVideo(); // keep classifying in a loop
+}
