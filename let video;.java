dbcfa1e7455749da012d2cc74114589a785e2c1e
Joan Lee <jlee2296@wisc.edu>
2025-04-14 20:57:10 -0500

    Update let video;.java

--- let video;.java
+++ let video;.java
@@ -1,1 +1,40 @@
+let video;
+let classifier;
+let label = 'waiting...';
+let confidence = 0;
 
+// STEP 1: Load the model!
+let modelURL = 'https://teachablemachine.withgoogle.com/models/lsBrn0MsP/';
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+function setup() {
+  createCanvas(640, 520);
+  video = createCapture(VIDEO);
+  video.size(640, 520);
+  video.hide();
+
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text(label + ' (' + nf(confidence * 100, 2, 1) + '%)', width / 2, height - 16);}
+
+// STEP 3: Get classification result
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
